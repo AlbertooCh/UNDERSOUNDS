d3b946137235096ddf9ab7bff71659e1979107cc
AlbertooCh <Albertochamizo05@gmail.com>
2025-02-28 09:57:45 +0100

    Reorganizacion

--- undersounds/urls.py
+++ undersounds/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import urls
 from music import urls
 from user import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+
     path('', include('music.urls')),
     path('', include('user.urls')),
 ]
