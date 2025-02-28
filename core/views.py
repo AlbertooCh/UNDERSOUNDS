8c65acb1ab91b30ca73d49bfdd6dea16eb2ae775
AlbertooCh <Albertochamizo05@gmail.com>
2025-02-28 09:57:45 +0100

    Reorganizacion

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def inicio(request):
-    return render(request, 'inicio.html')
-# Create your views here.
-
-def contacto(request):
-    return render(request, 'contacto.html')
