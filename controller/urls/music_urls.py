7e67b95ec965734b851dc4f190b2154e7da5ac1a
AlbertooCh <Albertochamizo05@gmail.com>
2025-04-11 11:36:27 +0200

    Musica terminada

--- controller/urls/music_urls.py
+++ controller/urls/music_urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from view.music import views
 
 urlpatterns = [
+    # Catálogo y detalles
     path('catalogo/', views.catalogo, name='catalogo'),
-    path('music_detail/', views.music_detail_redirect, name='music_detail'),  # optional
+    path('music_detail/', views.music_detail_redirect, name='music_detail'),
     path('music_detail/<int:id>/', views.music_detail, name='music_detail_id'),
-    path('artist_detail/', views.artist_detail, name='artist_detail'),
 
-    # CRUD Music
-    path('music/add/', views.add_song, name='add_song'),
-    path('music/edit/<int:song_id>/', views.edit_song, name='edit_song'),
-    path('music/delete/<int:song_id>/', views.delete_song, name='delete_song'),
-    path('artist/panel/', views.artist_panel, name='artist_panel'), # panel for artist to see his all songs
+    # Artista
+    path('artist_detail/', login_required(views.artist_detail), name='artist_detail'),
+    path('artist/panel/', login_required(views.artist_panel), name='artist_panel'),
 
+    # CRUD de canciones (protegidas por login y rol de artista)
+    path('music/add/', login_required(views.add_song), name='add_song'),
+    path('music/edit/<int:song_id>/', login_required(views.edit_song), name='edit_song'),
+    path('music/delete/<int:song_id>/', login_required(views.delete_song), name='delete_song'),
 ]
